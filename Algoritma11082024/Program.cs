512f150bf15467b9cc824b0a939c7d800835256f
Hp <Hp@MERVESU>
2024-09-09 12:58:16 +0300

    Add project files.

---
+++ Algoritma11082024/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritma11082024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            kisi k1 = new kisi("niloya", "bahce", "055555555555");
+            k1.kisiBilgisiBas();
+            Console.ReadKey();
+        }
+    }
+    }
---
+++ Algoritma11082024/kisi.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritma11082024
+{
+    class kisi
+    {
+        
+        private string ad, soyad, telno;
+
+        public kisi(string Ad, string Soyad, string telNo)
+        {
+            this.ad = Ad;
+            this.soyad = Soyad;
+            this.telno = telNo;
+        }
+        public void kisiBilgisiBas()
+        {
+            Console.WriteLine(ad + " " + soyad + " " + telno);
+        }
+        
+    }
+}
